914d0213692677aad54707cffebad810bcb9f0b6
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-17 22:40:38 +0530

    setting up passport js

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true,
+    },
+    displayName: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    image: { type: String },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    }
+});
+
+module.exports = mongoose.Model('User', UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 // Add "type": "module" in package.json to use ES Modules
 import path from 'path';
 import express from 'express';
+import passport from 'passport';
+import session from 'express-session';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
+
 // Configuring environment variables with .dotenv
 dotenv.config();
+
+// Passport config
+require('./config/passport')(passport);
 
 // Connecting to MongoDB database
 connectDB();
@@ -13,6 +19,17 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+// Sessions
+app.use(session({
+    secret: 'hdfhhw72379ohdna',
+    resave: false, // don't save a session if nothing is modified
+    saveUninitialized: false, // don't create a session until something is stored
+}));
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serving static files
 const __dirname = path.resolve();
