8c3cb498c66d4f37c370ce1ec9aa9423bbc24a96
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-18 13:30:05 +0530

    serializing user using passport

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import e from 'express';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/userModel.js';
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
 
 dotenv.config();
 
@@ -10,13 +15,25 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback', // Callback URL after user grants permissions to the google
 }, async (accessToken, refreshToken, profile, done) => {
-    const user = new User({
-        googleId: profile.id,
-        displayName: profile.displayName,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        image: profile.photos[0].value,
-    });
 
-    await user.save();
+    const user = await User.findOne({ googleId: profile.id });
+
+    if (user) {
+        // we already have a record
+        done(null, user);
+    } else {
+        // don't have a record, create a new one
+        const newUser = await new User({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value,
+        }).save();
+
+        if (newUser) {
+            done(null, newUser);
+        }
+    }
+
 }));
