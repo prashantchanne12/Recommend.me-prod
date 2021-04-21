e9fb532c523a74aa604ac3c873b62241f64c380a
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-21 11:55:08 +0530

    minor changes

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,9 +2,14 @@ import asyncHandler from 'express-async-handler';
 import User from "../models/userModel.js";
 
 export const protect = asyncHandler(async (req, res, next) => {
+    // PRODUCTION
+    // if (!req.user) {
+    //     res.status(401);
+    //     throw new Error('Not Auhtorized')
+    // }
 
+    // POSTMAN TESTING
     const user = await User.findById('607c4e674597e65634cbcc82');
-    console.log(user);
 
     req.user = user;
 
