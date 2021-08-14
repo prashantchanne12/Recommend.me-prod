98d499d86da04d52bf6fb57fe3acf54ba6c107d9
poonam-bot7 <prashantchanne121@gmail.com>
2021-08-14 19:23:01 +0530

    created notification model

---
+++ backend/models/notificationModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userRef = {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+};
+
+const recommendationRef = {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'RecommendList',
+};
+
+const notificationSchema = new mongoose.Schema({
+
+    type: {
+        type: String,
+        required: true,
+    },
+    recommendation: recommendationRef,
+    userId: userRef,
+    data: {
+        type: String
+    }
+
+}, {
+    timestamps: true,
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+export default Notification;
